2e364c84f3c895f48b3128765d9c28d6242e7b15
Mark van Vliet <mark.v.v@outlook.com>
2023-12-18 15:18:56 +0100

    genericSparqlQuery is now a module

--- Footprint-Website/src/js/main.js
+++ Footprint-Website/src/js/main.js
@@ -1,6 +1,7 @@
 import { createRadialClusterTreeChart } from './d3/radialClusterTree.js';
 import { createRadialTidyTreeChart } from './d3/radialTidyTree.js';
 import { transformSPARQLtoD3Hierarchie } from './d3/sparqlToD3Hierarchie.js';
+import { genericSPARQLQuery } from './sparql/genericSPARQLQuery.js';
 
 // Fills the form based on type of footprint input.
 document
@@ -164,24 +165,6 @@ async function createEntityDropDownList(footprintType) {
     } catch (error) {
       console.error('Error:', error);
     }
-  }
-}
-
-async function genericSPARQLQuery(query) {
-  try {
-    const response = await fetch(
-      `http://localhost:7200/repositories/EO4GEOKG?query=${encodeURIComponent(
-        query
-      )}`,
-      {
-        method: 'GET',
-        headers: { Accept: 'application/sparql-results+json' },
-      }
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Fetch error:', error);
   }
 }
 
