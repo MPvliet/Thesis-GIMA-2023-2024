c93d387e9db78f8fd5208be6e84fe41e2ebbd3dc
Mark van Vliet <mark.v.v@outlook.com>
2023-12-23 17:50:42 +0100

    Added delete and insert SPARQL scripts, which communicate with the KG management forms.

--- Footprint-Website/src/js/knowledgeGraphManagement.js
+++ Footprint-Website/src/js/knowledgeGraphManagement.js
@@ -2,10 +2,9 @@ import { getAllOrganisations } from './sparql/getAllOrganisations.js';
 import { getAllExpertsFromOrganisation } from './sparql/getAllExpertsFromOrganisation.js';
 import { getAllExpertiseFromExpert } from './sparql/getAllExpertiseFromExpert.js';
 import { getAllMissingExpertiseFromExpert } from './sparql/getAllMissingExpertiseFromExpert.js';
-import {
-  addExpertiseAnnotation,
-  deleteExpertiseAnnotation,
-} from './sparql/AddOrDeleteExpertiseInKG.js';
+import { genericSPARQLQuery } from './sparql/genericSPARQLQuery.js';
+import { insertSPARQLStatement } from './sparql/insertSPARQLStatement.js';
+import { deleteSPARQLStatement } from './sparql/deleteSPARQLStatement.js';
 
 // Fills the dropdownList with all available Organisations once the webpage is fully loaded
 document.addEventListener(
@@ -44,6 +43,19 @@ document
   .getElementById('submitButton-Add')
   .addEventListener('click', async event => {
     event.preventDefault(); // Without this the page refreshes once I click the submit button, but I want JS to process form input, not refresh.
+    const expert = document.getElementById('dropdownExperts-Add').value;
+    const concept = document.getElementById('dropdownConcepts-Add').value;
+    addExpertiseAnnotation(expert, concept);
+  });
+
+// Processes what happens once you click Delete
+document
+  .getElementById('submitButton-Delete')
+  .addEventListener('click', async event => {
+    event.preventDefault(); // Without this the page refreshes once I click the submit button, but I want JS to process form input, not refresh.
+    const expert = document.getElementById('dropdownExperts-Delete').value;
+    const concept = document.getElementById('dropdownConcepts-Delete').value;
+    deleteExpertiseAnnotation(expert, concept);
   });
 
 // Fills the Dropdown menu based on the information returned by the SPARQL query
@@ -91,4 +103,102 @@ async function populateConceptsAddDropDown(expert) {
     options += '<option>' + concept + '</option>';
   }
   document.getElementById('dropdownConcepts-Add').innerHTML = options;
+}
+
+async function addExpertiseAnnotation(expertName, conceptName) {
+  const expertID = await retrieveExpertID(expertName);
+  const conceptID = await retrieveConceptID(conceptName);
+  const insertQuery = `
+  PREFIX eo4geo: <https://bok.eo4geo.eu/>
+  PREFIX boka: <http://example.org/BOKA/>
+  INSERT DATA {
+    GRAPH <https://bok.eo4geo.eu/applications> {
+        eo4geo:${conceptID} boka:personWithKnowledge eo4geo:${expertID}
+    }
+  }
+  `;
+
+  const response = await insertSPARQLStatement(insertQuery);
+  if (response === 204) {
+    alert(
+      `${conceptName} succesvol toegevoegd als expertise aan ${expertName}`
+    );
+  }
+}
+
+async function deleteExpertiseAnnotation(expertName, conceptName) {
+  const expertID = await retrieveExpertID(expertName);
+  const conceptID = await retrieveConceptID(conceptName);
+  const deleteQuery = `
+  PREFIX eo4geo: <https://bok.eo4geo.eu/>
+  PREFIX boka: <http://example.org/BOKA/>
+  DELETE DATA {
+    GRAPH <https://bok.eo4geo.eu/applications> {
+        eo4geo:${conceptID} boka:personWithKnowledge eo4geo:${expertID}
+    }
+  }
+  `;
+
+  const response = await deleteSPARQLStatement(deleteQuery);
+  if (response === 204) {
+    alert(
+      `${conceptName} succesvol verwijderd als expertise van ${expertName}`
+    );
+  }
+}
+
+async function retrieveExpertID(expertName) {
+  const query = `
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX boka: <http://example.org/BOKA/>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+  select ?expertURI 
+  where { 
+    ?expertURI rdf:type boka:Expert ;
+              foaf:name ?expertName .
+      filter(CONTAINS(str(?expertName), "${expertName}")) 
+  }
+  `;
+
+  try {
+    const data = await genericSPARQLQuery(query);
+    const expertURI = data['results']['bindings'][0].expertURI.value;
+    const expertID = expertURI.replace(
+      new RegExp(`^https://bok.eo4geo.eu/`),
+      ''
+    );
+    return expertID;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return [];
+  }
+}
+
+async function retrieveConceptID(conceptName) {
+  const query = `
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX obok: <http://example.org/OBOK/>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+  select ?conceptURI 
+  where { 
+    ?conceptURI rdf:type obok:Concept ;
+              rdfs:label ?conceptName .
+      filter(CONTAINS(str(?conceptName), "${conceptName}")) 
+  }
+  `;
+
+  try {
+    const data = await genericSPARQLQuery(query);
+    const conceptURI = data['results']['bindings'][0].conceptURI.value;
+    const conceptID = conceptURI.replace(
+      new RegExp(`^https://bok.eo4geo.eu/`),
+      ''
+    );
+    return conceptID;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return [];
+  }
 }
