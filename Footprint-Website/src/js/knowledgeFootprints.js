fd28fa0f866c29d93bba188ef6a0c576fc07c348
Mark van Vliet <mark.v.v@outlook.com>
2024-01-19 11:32:07 +0100

    Added a free input field to choose organisations/individuals and also a small detail sections wich orgs/ individuals are incldued

--- Footprint-Website/src/js/knowledgeFootprints.js
+++ Footprint-Website/src/js/knowledgeFootprints.js
@@ -29,12 +29,16 @@ document
     searchConceptInD3Vis(event.target.value);
   });
 
+/* Enable this if we want to go back to a predefined list of all org or persons in the KG.
+
 // Fills the HTML form based on type of footprint input.
 document
   .getElementById('typeOfFootprintDropDown')
   .addEventListener('change', function () {
     createEntityDropDownList(this.value);
   });
+
+*/
 
 // Processes what happens once you click Generate Footprint
 document
@@ -139,6 +143,32 @@ document
       ORDER BY (?conceptName)
       `;
     }
+    let queryIncludedEntities;
+    if (footprintType === 'Individual') {
+      queryIncludedEntities = `
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX boka: <http://example.org/BOKA/>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+    SELECT DISTINCT ?expertName WHERE {
+      ?expertURI rdf:type boka:Expert;
+        foaf:name ?expertName .
+      FILTER(CONTAINS(STR(?expertName), "${footprintEntity}"))
+    }
+    `;
+    } else if (footprintType === 'Organisational') {
+      queryIncludedEntities = `
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX org: <http://www.w3.org/ns/org#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+    SELECT DISTINCT ?organisationName WHERE {
+      ?organisationURI rdf:type org:Organization;
+        rdfs:label ?organisationName.
+      FILTER(CONTAINS(STR(?organisationName), "${footprintEntity}"))
+    }
+    `;
+    }
 
     // Defines which function to call to generate the chosen visualisationType
     const visualisationFunction = {
@@ -154,6 +184,35 @@ document
     try {
       const sparqlResponse = await genericSPARQLQuery(query);
       const data = transformSPARQLtoD3Hierarchie(sparqlResponse);
+
+      const uniqueEntities = await genericSPARQLQuery(queryIncludedEntities);
+
+      let includedEntityList = '<ul style="margin-top: 0;">';
+      uniqueEntities.results.bindings.forEach(entitiy => {
+        if (footprintType === 'Individual') {
+          includedEntityList += `<li>${entitiy.expertName.value}</li>`;
+        } else if (footprintType === 'Organisational') {
+          includedEntityList += `<li>${entitiy.organisationName.value}</li>`;
+        }
+      });
+      includedEntityList += '</ul>';
+
+      let includedEntities;
+      if (footprintType === 'Individual') {
+        includedEntities = `
+      <h4 style="margin-bottom: 1;">Included individuals in this query</h4>
+      ${includedEntityList}
+      `;
+      } else if (footprintType === 'Organisational') {
+        includedEntities = `
+      <h4 style="margin-bottom: 1;">Included organisations  in this query</h4>
+      ${includedEntityList}
+      `;
+      }
+
+      document.getElementById('includedEntitySection').innerHTML =
+        includedEntities;
+
       visualisationFunction[visualisationType](data);
     } catch (error) {
       console.error('Error creating D3 visualisation: ', error);
@@ -161,6 +220,8 @@ document
         'Error creating D3 visualisation: ' + error.message;
     }
   });
+
+/* Enable this if we want to go back to a predefined list of all org or persons in the KG.
 
 async function createEntityDropDownList(footprintType) {
   if (footprintType === 'Individual') {
@@ -194,3 +255,5 @@ function fillOrganisationAndPersonList(footprintType, list) {
     document.getElementById('dropdownFootprintEntity').innerHTML = options;
   }
 }
+
+*/
